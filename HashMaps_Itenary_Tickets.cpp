d70fd0d91c5a0a245d234947ebeaf661ac8db970
Armaan Singh Dhaliwal <117513972+Armaan-Dhaliwal@users.noreply.github.com>
2023-08-26 11:40:13 +0530

    Add files via upload

---
+++ HashMaps_Itenary_Tickets.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+using namespace std;
+
+int main(){
+    cout<<"Enter number of enteries: "<<endl;
+    int n;
+    cin>>n;
+    vector<pair<string,string>> vec;
+    for(int i=0;i<n;i++){
+        string a,b;
+        cin>>a;
+        cin>>b;
+        vec.push_back({a,b});
+    }
+
+    unordered_map<string,int> fMap;
+    unordered_map<string,string> lMap;
+    for(int i=0;i<n;i++){
+        fMap[vec[i].first]+=1;
+        fMap[vec[i].second]+=1;
+        lMap[vec[i].first]=vec[i].second;
+    }
+
+    string src, des;
+    for(auto i=fMap.begin();i!=fMap.end();i++){
+        if(i->second ==1){
+            if(lMap.count(i->first)>0){
+                src = i->first;
+            }
+            else{
+                des = i->first;
+            }
+        }
+    }
+    // cout<<src<<endl;
+    // cout<<des<<endl;
+
+    string output="";
+
+   for(int i=0;i<n;i++){
+    output+=src+" -> "+lMap[src]+" ";
+    src=lMap[src];
+   } 
+   cout<<"OUTPUT: "<<output<<endl;
+}
+
+// 4 Chennai Banglore Bombay Delhi Goa Chennai Delhi Goa
